c8a1a9350e7da0d8dfa404c9c6ab0fb4cb1ecad5
Eldert Grootenboer <eldert@eldert.org>
2020-09-06 15:34:53 +0200

    Added demo for security misconfiguration

--- Assets/Code/Web API/AssetManagementApi/Controllers/RepairsController.cs
+++ Assets/Code/Web API/AssetManagementApi/Controllers/RepairsController.cs
@@ -70,8 +70,6 @@ namespace OrderSystemApi.Controllers
         }
 
         // PUT: api/Repairs/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutRepair(long id, Repair repair)
         {
@@ -82,28 +80,12 @@ namespace OrderSystemApi.Controllers
 
             _context.Entry(repair).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!RepairExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }
 
         // POST: api/Repairs
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<Repair>> PostRepair(Repair repair)
         {
