91c8d82ba86641eb8a769ab3ff2c7f82b96babbb
Eldert Grootenboer <eldert@eldert.net>
2020-09-22 21:21:41 +0200

    Provide easy way of deplo

--- Assets/Code/IaC/1. create-app-registrations-for-oauth-demo.ps1
+++ Assets/Code/IaC/1. create-app-registrations-for-oauth-demo.ps1
@@ -1,11 +1,15 @@
 # What we will be doing in this script.
-#   1. Create a app registration for the API
-#   2. Create a app registration for the consumer
+#   1. Create a app registration for the API, if not exists
+#   2. Create a app registration for the consumer, if not exists
+#   3. Create a resource group
+#   4. Deploy Azure services
 
 $subscriptionName = "Visual Studio Enterprise"
 $apiAppRegistrationName = "api-app-registration-api-platform-security"
 $clientAppRegistrationName = "client-app-registration-api-platform-security"
-$apiManagementInstanceName = "apim-api-platform-security"
+$apiManagementInstanceName = "apim-api-platform-security-2"
+$resourceGroupName = "rg-arm-in-a-serverless-world-2"
+$basePath = "C:\Users\elder\OneDrive\Sessions\Azure-Resource-Manager-in-A-Serverless-World"
 
 Get-AzSubscription -SubscriptionName $subscriptionName | Set-AzContext
 
@@ -18,3 +22,6 @@ if (-not $apiAppRegistrationExists) {
 if (-not $clientAppRegistrationExists) {
     New-AzADApplication -DisplayName $clientAppRegistrationName -IdentifierUris "api://$clientAppRegistrationName"
 }
+
+New-AzResourceGroup -Name $resourceGroupName -Location 'West Europe' -Tag @{CreationDate=[DateTime]::UtcNow.ToString(); Project="Azure Resource Manager In A Serverless World"; Purpose="Session"}
+New-AzResourceGroupDeployment -Name Demo2 -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\Assets\Code\IaC\azuredeploy.json"
