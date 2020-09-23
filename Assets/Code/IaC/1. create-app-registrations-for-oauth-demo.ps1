6526d958a7ed77ea7b1d91ceea1c8609519b5d7a
Eldert Grootenboer <eldert@eldert.net>
2020-09-23 13:43:44 +0200

    Updated templates for APIM OAuth2 authentication

--- Assets/Code/IaC/1. create-app-registrations-for-oauth-demo.ps1
+++ Assets/Code/IaC/1. create-app-registrations-for-oauth-demo.ps1
@@ -1,15 +1,16 @@
+# Prerequisites
+#   1. Create app registrations for APIM OAuth2 authentication, as described on https://bit.ly/api-management-oauth-aad
+
 # What we will be doing in this script.
-#   1. Create a app registration for the API, if not exists
-#   2. Create a app registration for the consumer, if not exists
-#   3. Create a resource group
-#   4. Deploy Azure services
+#   1. Retrieve app registrations
+#   2. Create a resource group
+#   3. Deploy Azure services
 
 # Update these according to the environment
 $subscriptionName = "Visual Studio Enterprise"
 $apiAppRegistrationName = "api-app-registration-api-platform-security"
-$clientAppRegistrationName = "client-app-registration-api-platform-security"
-$apiManagementInstanceName = "apim-api-platform-security-3"
 $resourceGroupName = "rg-rg-api-platform-security-2"
+$appServiceName = "app-api-platform-security-2"
 $basePath = "C:\Users\elder\OneDrive\Sessions\Building-better-security-for-your-API-platform-using-Azure-API-Management"
 $settingsPath = "$basePath\.vscode\settings.json"
 $administratorEmail = "me@eldert.net"
@@ -21,20 +22,22 @@ $basicAuthenticationPassword = (Get-Content -Path $settingsPath | ConvertFrom-Js
 # Login to Azure
 Get-AzSubscription -SubscriptionName $subscriptionName | Set-AzContext
 
-# Create application registrations if they do not yet exist
-# These are used in the OAuth flows, and need to be updated in the .vscode\settings.json file when created
-$apiAppRegistrationExists = Get-AzADApplication -DisplayName $apiAppRegistrationName
-$clientAppRegistrationExists = Get-AzADApplication -DisplayName $clientAppRegistrationName
-
-if (-not $apiAppRegistrationExists) {
-    New-AzADApplication -DisplayName $apiAppRegistrationName -IdentifierUris "api://$apiManagementInstanceName"
-}
-if (-not $clientAppRegistrationExists) {
-    New-AzADApplication -DisplayName $clientAppRegistrationName -IdentifierUris "api://$clientAppRegistrationName"
-}
+# Retrieve application registration for the API
+$apiAppRegistration = Get-AzADApplication -DisplayName $apiAppRegistrationName
 
 # Create the resource group and deploy the resources
 New-AzResourceGroup -Name $resourceGroupName -Location 'West Europe' -Tag @{CreationDate=[DateTime]::UtcNow.ToString(); Project="Building better security for your API platform using Azure API Management"; Purpose="Session"}
-New-AzResourceGroupDeployment -Name Demo -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\Assets\Code\IaC\azuredeploy.json" -administratorObjectId $administratorObjectId -basicAuthenticationPassword $basicAuthenticationPassword
+New-AzResourceGroupDeployment -Name Demo -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\Assets\Code\IaC\azuredeploy.json" -administratorObjectId $administratorObjectId -basicAuthenticationPassword $basicAuthenticationPassword -apiAppRegistrationApplicationId $apiAppRegistration.ApplicationId
 
-New-AzResourceGroupDeployment -Name Demo -ResourceGroupName $resourceGroupName -TemplateFile "$basePath\Assets\Code\IaC\api-management-api-7-logging.json"
+# Deploy contents of the App Service
+dotnet publish -c Release -o "$basePath\Assets\Code\Web API\AssetManagementApi\publish"
+Compress-Archive -Path "$basePath\Assets\Code\Web API\AssetManagementApi\publish\*" -DestinationPath "$basePath\Assets\Code\Web API\AssetManagementApi\Deployment.zip"
+Publish-AzWebapp -ResourceGroupName $resourceGroupName -Name $appServiceName -ArchivePath "$basePath\Assets\Code\Web API\AssetManagementApi\Deployment.zip"
+Remove-Item "$basePath\Assets\Code\Web API\AssetManagementApi\Deployment.zip"
+
+# Optional for debugging, loops through each local file individually
+#Get-ChildItem "$basePath\Assets\Code\IaC" -Filter *.json | 
+#Foreach-Object {
+#    Write-Output "Deploying: " $_.FullName
+#    New-AzResourceGroupDeployment -Name Demo -ResourceGroupName $resourceGroupName -TemplateFile $_.FullName -ErrorAction Ignore
+#}
